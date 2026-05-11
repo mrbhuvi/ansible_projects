73967433471b7e4da0b55e0473d6cf225fa4211b
mrbhuvi <shingadebhausaheb1@gmail.com>
2026-05-11 10:53:55 +0530

    added steps

--- terraform-ansible-project/modules/ec2/variables.tf
+++ terraform-ansible-project/modules/ec2/variables.tf
@@ -3,3 +3,5 @@ variable "ami_id" {}
 variable "instance_type" {}
 variable "key_name" {}
 variable "sg_id" {}
+
+# it is variable because we will use it in the ansible inventory file to connect to the instances
